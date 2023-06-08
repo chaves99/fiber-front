378b2ad59e2fcdacc1bd86c29036e17028044b9d
chaves99 <viniciusbaleia1999@gmail.com>
2023-06-08 17:58:00 -0300

    Some little changes.

--- src/app/core/service/local-storage.service.ts
+++ src/app/core/service/local-storage.service.ts
@@ -5,8 +5,10 @@ import { UserResponseModel } from "@core/models/user.model";
 @Injectable({ providedIn: 'root' })
 export class LocalStorageService {
 
+    private readonly key: string = "token";
+
     getloggedUser(): LoginResponseModel | null {
-        let token = localStorage.getItem('token');
+        let token = localStorage.getItem(this.key);
         if (token !== null) {
             let model: LoginResponseModel = JSON.parse(token);
             return model;
@@ -15,7 +17,12 @@ export class LocalStorageService {
     }
 
     addLoggedUser(user: UserResponseModel) {
-        localStorage.setItem("token", JSON.stringify(user));
+        localStorage.setItem(this.key, JSON.stringify(user));
+    }
+
+    logOut(): void {
+        localStorage.removeItem(this.key);
+        localStorage.clear();
     }
 
 }
