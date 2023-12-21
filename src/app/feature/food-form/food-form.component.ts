4751e1e3f8bdd4c160776c270c2e9361b8667732
chaves99 <viniciusbaleia1999@gmail.com>
2023-12-21 11:57:38 -0300

    Implementing food register form.

--- src/app/feature/food-form/food-form.component.ts
+++ src/app/feature/food-form/food-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FoodModel } from '@core/models/food.model';
+import { MenuListEnum, getUrlByType } from '@core/models/menu-list.model';
+import { FoodService } from '@core/services/http/food.service';
 
 @Component({
   selector: 'app-food-form',
@@ -18,8 +22,26 @@ export class FoodFormComponent implements OnInit {
     fat: new FormControl(),
   });
 
+  constructor(
+    private foodService: FoodService,
+    private router: Router
+  ){}
+
   ngOnInit(): void {
     
+  }
+
+  submit() {
+    console.log(this.foodFormGroup.value);
+    let foodModel: FoodModel = {
+      name: this.foodFormGroup.value.foodName,
+      baseQuantity: this.foodFormGroup.value.quantityBase,
+      calories: this.foodFormGroup.value.calories,
+      carbohydrate: this.foodFormGroup.value.carbs,
+      fat: this.foodFormGroup.value.fat,
+      fiber: this.foodFormGroup.value.fiber
+    };
+    this.foodService.create(foodModel).subscribe(fm => this.router.navigateByUrl(`feature/${getUrlByType(MenuListEnum.FOOD)}`));
   }
 
 }
